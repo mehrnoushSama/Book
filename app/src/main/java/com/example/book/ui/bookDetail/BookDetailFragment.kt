83fc75204c14c323dadede52af5f9422f1378870
me.samavati <mehrnoushsama@gmail.com>
2022-02-07 10:40:21 +0330

    Add ViewModel for BookDetail Fragment

--- app/src/main/java/com/example/book/ui/bookDetail/BookDetailFragment.kt
+++ app/src/main/java/com/example/book/ui/bookDetail/BookDetailFragment.kt
@@ -5,17 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.example.book.R
+import com.example.book.databinding.FragmentBookDetailBinding
 
 
 class BookDetailFragment : Fragment() {
+
+    private var _binding: FragmentBookDetailBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var viewModel: BookDetailViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_book_detail, container, false)
+        viewModel = ViewModelProvider(this).get(BookDetailViewModel::class.java)
+        _binding = FragmentBookDetailBinding.inflate(inflater, container, false)
+        val view = binding.root
+        //return inflater.inflate(R.layout.fragment_book_detail, container, false)
+        return view
     }
 
 }
---
+++ app/src/main/java/com/example/book/ui/bookDetail/BookDetailViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.book.ui.bookDetail
+
+import androidx.lifecycle.ViewModel
+
+class BookDetailViewModel : ViewModel() {
+
+
+}
